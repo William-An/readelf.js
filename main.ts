405fb4b715250b0aa581327115ebb617f83e877f
William An <china_aisa@live.com>
2023-10-11 13:54:48 -0400

    Modify program header parsing to use size mapping

--- main.ts
+++ main.ts
@@ -2,6 +2,58 @@ import { assert } from "console";
 
 type integer = number | bigint;
 
+/**
+ * Field <-> size mapping for fields in type T
+ */
+type ObjectFieldSizeMap<T extends object> = FieldSizeMapping<T>[];
+
+type ProgramHeaderTableEntry = {
+    p_type: integer;
+    p_flags: integer;
+    p_offset: integer;
+    p_vaddr: integer;
+    p_paddr: integer;
+    p_filesz: integer;
+    p_memsz: integer;
+    p_align: integer;
+}
+
+type SectionHeaderTableEntry = {
+    sh_name: integer;
+    sh_type: integer;
+    sh_flags: integer;
+    sh_addr: integer;
+    sh_offset: integer;
+    sh_size: integer;
+    sh_link: integer;
+    sh_info: integer;
+    sh_addralign: integer;
+    sh_entsize: integer;
+}
+
+/**
+ * Index enum for elf file header
+ */
+export enum EI_INDEX {
+    EI_MAG0 = 0,
+    EI_MAG1,
+    EI_MAG2,
+    EI_MAG3,
+    EI_CLASS,
+    EI_DATA,
+    EI_VERSION,
+    EI_OSABIT,
+    EI_ABIVERSION,
+    EI_PAD,
+    EI_PAD0 = EI_PAD,
+    EI_PAD1,
+    EI_PAD2,
+    EI_PAD3,
+    EI_PAD4,
+    EI_PAD5,
+    EI_PAD6,
+};
+
 class ELFInfo {
     bit: "32" | "64";
     endianness: "little" | "big";
@@ -76,29 +128,6 @@ class ELFHeader {
         return res;
     }
 }
-
-/**
- * Index enum for elf file header
- */
-export enum EI_INDEX {
-    EI_MAG0 = 0,
-    EI_MAG1,
-    EI_MAG2,
-    EI_MAG3,
-    EI_CLASS,
-    EI_DATA,
-    EI_VERSION,
-    EI_OSABIT,
-    EI_ABIVERSION,
-    EI_PAD,
-    EI_PAD0 = EI_PAD,
-    EI_PAD1,
-    EI_PAD2,
-    EI_PAD3,
-    EI_PAD4,
-    EI_PAD5,
-    EI_PAD6,
-};
 
 class ELFFileHeader extends ELFHeader {
     readonly e_ident_size = 0x10;
@@ -205,55 +234,32 @@ type FieldSizeMapping<T> = {
     size: number;
 }
 
-/**
- * Field <-> size mapping for fields in type T
- */
-type ObjectFieldSizeMap<T extends object> = FieldSizeMapping<T>[];
-
-type phTableEntry = {
-    p_type: integer;
-    p_flags: integer;
-    p_offset: integer;
-    p_vaddr: integer;
-    p_paddr: integer;
-    p_filesz: integer;
-    p_memsz: integer;
-    p_align: integer;
-}
-
-type SectionHeaderTableEntry = {
-    sh_name: integer;
-    sh_type: integer;
-    sh_flags: integer;
-    sh_addr: integer;
-    sh_offset: integer;
-    sh_size: integer;
-    sh_link: integer;
-    sh_info: integer;
-    sh_addralign: integer;
-    sh_entsize: integer;
-}
-
 class ELFProgramHeader extends ELFHeader {
-    phTable: Array<phTableEntry>;
+    phTable: Array<ProgramHeaderTableEntry>;
     phEntries: number;
     phoff: integer;
+    readonly phEntrySizeMapping32: ObjectFieldSizeMap<ProgramHeaderTableEntry> = [
+        { field: "p_type", size: 0x4 },
+        { field: "p_offset", size: 0x4 },
+        { field: "p_vaddr", size: 0x4 },
+        { field: "p_paddr", size: 0x4 },
+        { field: "p_filesz", size: 0x4 },
+        { field: "p_memsz", size: 0x4 },
+        { field: "p_flags", size: 0x4 },
+        { field: "p_align", size: 0x4 },
+    ];
+    readonly phEntrySizeMapping64: ObjectFieldSizeMap<ProgramHeaderTableEntry> = [
+        { field: "p_type", size: 0x4 },
+        { field: "p_flags", size: 0x4 },
+        { field: "p_offset", size: 0x8 },
+        { field: "p_vaddr", size: 0x8 },
+        { field: "p_paddr", size: 0x8 },
+        { field: "p_filesz", size: 0x8 },
+        { field: "p_memsz", size: 0x8 },
+        { field: "p_align", size: 0x8 },
+    ];
     readonly phEntry_size_32 = 0x20;
     readonly phEntry_size_64 = 0x38;
-    readonly p_type_size = 0x4;
-    readonly p_flags_size = 0x4;
-    readonly p_offset_size_32 = 0x4;
-    readonly p_vaddr_size_32 = 0x4;
-    readonly p_paddr_size_32 = 0x4;
-    readonly p_filesz_size_32 = 0x4;
-    readonly p_memsz_size_32 = 0x4;
-    readonly p_align_size_32 = 0x4;
-    readonly p_offset_size_64 = 0x8;
-    readonly p_vaddr_size_64 = 0x8;
-    readonly p_paddr_size_64 = 0x8;
-    readonly p_filesz_size_64 = 0x8;
-    readonly p_memsz_size_64 = 0x8;
-    readonly p_align_size_64 = 0x8;
 
 
     constructor(binary: Buffer, bit: "32" | "64", endianess: "big" | "little",
@@ -263,7 +269,7 @@ class ELFProgramHeader extends ELFHeader {
         this.endianness = endianess;
         this.phEntries = phEntries;
         this.phoff = phoff;
-        this.phTable = Array<phTableEntry>(this.phEntries);
+        this.phTable = Array<ProgramHeaderTableEntry>(this.phEntries);
 
         // Fill up table entry from offset
         let offset = this.phoff;
@@ -272,8 +278,8 @@ class ELFProgramHeader extends ELFHeader {
         }
     }
 
-    readEntry(start_offset: number): [phTableEntry, number] {
-        let entry: phTableEntry = {
+    readEntry(start_offset: number): [ProgramHeaderTableEntry, number] {
+        let entry: ProgramHeaderTableEntry = {
             p_type: 0,
             p_flags: 0,
             p_offset: 0,
@@ -286,49 +292,19 @@ class ELFProgramHeader extends ELFHeader {
 
         // Read 1 entry from binary with offset 
         let offset = start_offset;
-        entry.p_type = this.readUIntVariousSize(offset, this.p_type_size) as number;
-        offset += this.p_type_size;
-        if (this.bit === "32") {
-            // 32-bit arch
-            entry.p_offset = this.readUIntVariousSize(offset, this.p_offset_size_32);
-            offset += this.p_offset_size_32;
-            entry.p_vaddr = this.readUIntVariousSize(offset, this.p_vaddr_size_32);
-            offset += this.p_vaddr_size_32;
-            entry.p_paddr = this.readUIntVariousSize(offset, this.p_paddr_size_32);
-            offset += this.p_paddr_size_32;
-            entry.p_filesz = this.readUIntVariousSize(offset, this.p_filesz_size_32);
-            offset += this.p_filesz_size_32;
-            entry.p_memsz = this.readUIntVariousSize(offset, this.p_memsz_size_32);
-            offset += this.p_memsz_size_32;
-            entry.p_flags = this.readUIntVariousSize(offset, this.p_flags_size) as number;
-            offset += this.p_flags_size;
-            entry.p_align = this.readUIntVariousSize(offset, this.p_align_size_32);
-            offset += this.p_align_size_32;
+        let mapping = this.bit === "32" ? this.phEntrySizeMapping32 : this.phEntrySizeMapping64;
+        mapping.forEach((mapping) => {
+            let field = mapping.field;
+            let size = mapping.size;
+            entry[field] = this.readUIntVariousSize(offset, size);
+            offset += size;
+        });
 
-            // Check if size info matches
-            if (offset - start_offset !== this.phEntry_size_32)
-                throw Error(`Program header entry size mismatched! file header has ${this.phEntry_size_32} but actually is ${offset - start_offset}!`);
-        } else {
-            // 64-bit arch
-            entry.p_flags = this.readUIntVariousSize(offset, this.p_flags_size) as number;
-            offset += this.p_flags_size;
-            entry.p_offset = this.readUIntVariousSize(offset, this.p_offset_size_64);
-            offset += this.p_offset_size_64;
-            entry.p_vaddr = this.readUIntVariousSize(offset, this.p_vaddr_size_64);
-            offset += this.p_vaddr_size_64;
-            entry.p_paddr = this.readUIntVariousSize(offset, this.p_paddr_size_64);
-            offset += this.p_paddr_size_64;
-            entry.p_filesz = this.readUIntVariousSize(offset, this.p_filesz_size_64);
-            offset += this.p_filesz_size_64;
-            entry.p_memsz = this.readUIntVariousSize(offset, this.p_memsz_size_64);
-            offset += this.p_memsz_size_64;
-            entry.p_align = this.readUIntVariousSize(offset, this.p_align_size_64);
-            offset += this.p_align_size_64;
-            
-            // Check if size info matches
-            if (offset - start_offset !== this.phEntry_size_64)
-                throw Error(`Program header entry size mismatched! file header has ${this.phEntry_size_64} but actually is ${offset - start_offset}!`);
-        }
+        let diff = offset - start_offset;
+        if (this.bit === "32")
+            assert(diff === this.phEntry_size_32, `Entry size mismatched! Expect ${this.phEntry_size_32} but got ${diff}`);
+        else
+            assert(diff === this.phEntry_size_64, `Entry size mismatched! Expect ${this.phEntry_size_64} but got ${diff}`);
 
         return [entry, offset];
     }
@@ -337,8 +313,8 @@ class ELFProgramHeader extends ELFHeader {
 class ELFSectionHeader extends ELFHeader {
     shTable: Array<SectionHeaderTableEntry>;
     shEntries: number;
-    shoff: number | bigint;
-    readonly ShEntrySizeMapping32: ObjectFieldSizeMap<SectionHeaderTableEntry> = [
+    shoff: integer;
+    readonly shEntrySizeMapping32: ObjectFieldSizeMap<SectionHeaderTableEntry> = [
         { field: "sh_name", size: 0x4 },
         { field: "sh_type", size: 0x4 },
         { field: "sh_flags", size: 0x4 },
@@ -350,7 +326,7 @@ class ELFSectionHeader extends ELFHeader {
         { field: "sh_addralign", size: 0x4 },
         { field: "sh_entsize", size: 0x4 },
     ];
-    readonly ShEntrySizeMapping64: ObjectFieldSizeMap<SectionHeaderTableEntry> = [
+    readonly shEntrySizeMapping64: ObjectFieldSizeMap<SectionHeaderTableEntry> = [
         { field: "sh_name", size: 0x4 },
         { field: "sh_type", size: 0x4 },
         { field: "sh_flags", size: 0x8 },
@@ -362,7 +338,6 @@ class ELFSectionHeader extends ELFHeader {
         { field: "sh_addralign", size: 0x8 },
         { field: "sh_entsize", size: 0x8 },
     ];
-
     readonly shEntry_size_32 = 0x28;
     readonly shEntry_size_64 = 0x40;
 
@@ -397,7 +372,7 @@ class ELFSectionHeader extends ELFHeader {
             sh_entsize: 0x0,
         };
 
-        let mapping = this.bit === "32" ? this.ShEntrySizeMapping32 : this.ShEntrySizeMapping64;
+        let mapping = this.bit === "32" ? this.shEntrySizeMapping32 : this.shEntrySizeMapping64;
         mapping.forEach((mapping) => {
             let field = mapping.field;
             let size = mapping.size;
