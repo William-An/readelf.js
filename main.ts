bec3fd9b77f8fb6ecf6c7a051c2f22132a91d64a
William An <china_aisa@live.com>
2023-10-11 11:24:11 -0400

    Finish program header parser

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+import { assert } from "console";
+
 class ELFInfo {
     bit: "32" | "64";
     endianness: "little" | "big";
@@ -12,7 +14,8 @@ class ELFInfo {
         this.fileHeader = new ELFFileHeader(this.binary);
         this.bit = this.fileHeader.bit;
         this.endianness = this.fileHeader.endianness
-        this.progHeader = new ELFProgramHeader();
+        this.progHeader = new ELFProgramHeader(this.binary, this.bit, this.endianness, 
+            this.fileHeader.e_phnum, this.fileHeader.e_phoff);
         this.sectionHeader = new ELFSectionHeader();
     }
 };
@@ -26,6 +29,44 @@ class ELFHeader {
         this.binary = binary;
         this.bit = "32";
         this.endianness = "little";
+    }
+
+    readUIntVariousSize(offset: number, size: 1 | 2 | 4 | 8): number | bigint {
+        let buf = this.binary;
+        let endianness = this.endianness;
+        let res: number | bigint = 0;
+        if (endianness === "big") {
+            switch (size) {
+                case 1:
+                    res = buf.readUInt8(offset);
+                    break;
+                case 2:
+                    res = buf.readUInt16BE(offset);
+                    break;
+                case 4:
+                    res = buf.readUInt32BE(offset);
+                    break;
+                case 8:
+                    res = buf.readBigUInt64BE(offset);
+                    break;
+            }
+        } else {
+            switch (size) {
+                case 1:
+                    res = buf.readUInt8(offset);
+                    break;
+                case 2:
+                    res = buf.readUInt16LE(offset);
+                    break;
+                case 4:
+                    res = buf.readUInt32LE(offset);
+                    break;
+                case 8:
+                    res = buf.readBigUInt64LE(offset);
+                    break;
+            }
+        }
+        return res;
     }
 }
 
@@ -147,47 +188,117 @@ class ELFFileHeader extends ELFHeader {
         if (offset !== this.e_ehsize)
             throw Error(`Header size mismatched with what is in the header! Header has ${this.e_ehsize} but actually is ${offset}!`);
     }
-
-    readUIntVariousSize(offset: number, size: 1 | 2 | 4 | 8): number | bigint {
-        let buf = this.binary;
-        let endianness = this.endianness;
-        let res: number | bigint = 0;
-        if (endianness == "big") {
-            switch (size) {
-                case 1:
-                    res = buf.readUInt8(offset);
-                    break;
-                case 2:
-                    res = buf.readUInt16BE(offset);
-                    break;
-                case 4:
-                    res = buf.readUInt32BE(offset);
-                    break;
-                case 8:
-                    res = buf.readBigUInt64BE(offset);
-                    break;
-            }
-        } else {
-            switch (size) {
-                case 1:
-                    res = buf.readUInt8(offset);
-                    break;
-                case 2:
-                    res = buf.readUInt16LE(offset);
-                    break;
-                case 4:
-                    res = buf.readUInt32LE(offset);
-                    break;
-                case 8:
-                    res = buf.readBigUInt64LE(offset);
-                    break;
-            }
-        }
-        return res;
-    }
 };
 
-class ELFProgramHeader {
+type phTableEntry = {
+    p_type: number;
+    p_flags: number;
+    p_offset: number | bigint;
+    p_vaddr: number | bigint;
+    p_paddr: number | bigint;
+    p_filesz: number | bigint;
+    p_memsz: number | bigint;
+    p_align: number | bigint;
+}
+
+class ELFProgramHeader extends ELFHeader {
+    phTable: Array<phTableEntry>;
+    phEntries: number;
+    phoff: number | bigint;
+    readonly phEntry_size_32 = 0x20;
+    readonly phEntry_size_64 = 0x38;
+    readonly p_type_size = 0x4;
+    readonly p_flags_size = 0x4;
+    readonly p_offset_size_32 = 0x4;
+    readonly p_vaddr_size_32 = 0x4;
+    readonly p_paddr_size_32 = 0x4;
+    readonly p_filesz_size_32 = 0x4;
+    readonly p_memsz_size_32 = 0x4;
+    readonly p_align_size_32 = 0x4;
+    readonly p_offset_size_64 = 0x8;
+    readonly p_vaddr_size_64 = 0x8;
+    readonly p_paddr_size_64 = 0x8;
+    readonly p_filesz_size_64 = 0x8;
+    readonly p_memsz_size_64 = 0x8;
+    readonly p_align_size_64 = 0x8;
+
+
+    constructor(binary: Buffer, bit: "32" | "64", endianess: "big" | "little",
+        phEntries: number, phoff: number | bigint) {
+        super(binary);
+        this.bit = bit;
+        this.endianness = endianess;
+        this.phEntries = phEntries;
+        this.phoff = phoff;
+        this.phTable = Array<phTableEntry>(this.phEntries);
+
+        // Fill up table entry from offset
+        let offset = this.phoff;
+        for (let i = 0; i < this.phEntries; i++) {
+            [this.phTable[i], offset] = this.readEntry(Number(offset));
+        }
+    }
+
+    readEntry(start_offset: number): [phTableEntry, number] {
+        let entry: phTableEntry = {
+            p_type: 0,
+            p_flags: 0,
+            p_offset: 0,
+            p_vaddr: 0,
+            p_paddr: 0,
+            p_filesz: 0,
+            p_memsz: 0,
+            p_align: 0,
+        };
+
+        // Read 1 entry from binary with offset 
+        let offset = start_offset;
+        entry.p_type = this.readUIntVariousSize(offset, this.p_type_size) as number;
+        offset += this.p_type_size;
+        if (this.bit === "32") {
+            // 32-bit arch
+            entry.p_offset = this.readUIntVariousSize(offset, this.p_offset_size_32);
+            offset += this.p_offset_size_32;
+            entry.p_vaddr = this.readUIntVariousSize(offset, this.p_vaddr_size_32);
+            offset += this.p_vaddr_size_32;
+            entry.p_paddr = this.readUIntVariousSize(offset, this.p_paddr_size_32);
+            offset += this.p_paddr_size_32;
+            entry.p_filesz = this.readUIntVariousSize(offset, this.p_filesz_size_32);
+            offset += this.p_filesz_size_32;
+            entry.p_memsz = this.readUIntVariousSize(offset, this.p_memsz_size_32);
+            offset += this.p_memsz_size_32;
+            entry.p_flags = this.readUIntVariousSize(offset, this.p_flags_size) as number;
+            offset += this.p_flags_size;
+            entry.p_align = this.readUIntVariousSize(offset, this.p_align_size_32);
+            offset += this.p_align_size_32;
+
+            // Check if size info matches
+            if (offset - start_offset !== this.phEntry_size_32)
+                throw Error(`Program header entry size mismatched! file header has ${this.phEntry_size_32} but actually is ${offset - start_offset}!`);
+        } else {
+            // 64-bit arch
+            entry.p_flags = this.readUIntVariousSize(offset, this.p_flags_size) as number;
+            offset += this.p_flags_size;
+            entry.p_offset = this.readUIntVariousSize(offset, this.p_offset_size_64);
+            offset += this.p_offset_size_64;
+            entry.p_vaddr = this.readUIntVariousSize(offset, this.p_vaddr_size_64);
+            offset += this.p_vaddr_size_64;
+            entry.p_paddr = this.readUIntVariousSize(offset, this.p_paddr_size_64);
+            offset += this.p_paddr_size_64;
+            entry.p_filesz = this.readUIntVariousSize(offset, this.p_filesz_size_64);
+            offset += this.p_filesz_size_64;
+            entry.p_memsz = this.readUIntVariousSize(offset, this.p_memsz_size_64);
+            offset += this.p_memsz_size_64;
+            entry.p_align = this.readUIntVariousSize(offset, this.p_align_size_64);
+            offset += this.p_align_size_64;
+            
+            // Check if size info matches
+            if (offset - start_offset !== this.phEntry_size_64)
+                throw Error(`Program header entry size mismatched! file header has ${this.phEntry_size_64} but actually is ${offset - start_offset}!`);
+        }
+
+        return [entry, offset];
+    }
 };
 
 class ELFSectionHeader {
